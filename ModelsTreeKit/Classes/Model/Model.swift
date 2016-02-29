644ca36bc20098273653d38134db35f5a1f26f5c
Aleksey Chernish <achernish85@gmail.com>
2016-02-29 11:45:49 +0700

    wants remove child signal

--- ModelsTreeKit/Classes/Model/Model.swift
+++ ModelsTreeKit/Classes/Model/Model.swift
@@ -13,7 +13,7 @@ public class Model {
   public private(set) weak var parent: Model?
   
   public let pushChildSignal = Signal<Model>()
-  public let removeChildSignal = Signal<Model>()
+  public let wantsRemoveChildSignal = Signal<Model>()
   public let errorSignal = Signal<Error>()
   public let pool = AutodisposePool()
   
