c8afc654a5d6e4e8c9bbbc4bdcbf8505ccb1171b
Aleksey <achernish85@gmail.com>
2016-06-29 15:45:24 +0700

    put Jetpack into the repo

---
+++ JetPack/Signal+Validator.swift
@@ -0,0 +1,17 @@
+//
+//  Signal+Validator.swift
+//  ModelsTreeKit
+//
+//  Created by aleksey on 04.06.16.
+//  Copyright © 2016 aleksey chernish. All rights reserved.
+//
+
+import Foundation
+
+extension Signal {
+  
+  func filterValidWith(validator: (T -> Bool)) -> Signal<T> {
+    return self.filter { validator($0) }
+  }
+  
+}
