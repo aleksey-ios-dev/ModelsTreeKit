2baa3934d4d91e79d79ba953e3e5ad7fd212adb1
Aleksey <aleksey@MacBook-Pro-Aleksey.local>
2016-12-12 07:30:32 +0700

    two list adapters in test apps

--- ModelsTreeDemo/ModelsTreeDemo/ViewController.swift
+++ ModelsTreeDemo/ModelsTreeDemo/ViewController.swift
@@ -15,37 +15,30 @@ class ViewController: UIViewController {
   
   private var adapter: TableViewAdapter<Int>!
   private var dataAdapter: ObjectsDataSource<Int>!
-  let list = UnorderedList<Int>(parent: nil, objects: [1, 2])
+  let list1 = UnorderedList<Int>(parent: nil, objects: [1, 2])
+  let list2 = UnorderedList<Int>(parent: nil, objects: [-9, 10])
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    let listAdapter = UnorderedListDataAdapter<Int, String>(list: list)
-    listAdapter.groupContentsSortingCriteria = { $0 < $1 }
-    listAdapter.groupingCriteria = { return $0 > 3 ? "2" : "1" }
-    list.performUpdates {
-      $0.insert([3, 4, 5])
-    }
+    let listAdapter1 = UnorderedListDataAdapter<Int, String>(list: list1)
+    listAdapter1.groupContentsSortingCriteria = { $0 < $1 }
+    listAdapter1.groupingCriteria = { return $0 > 3 ? "2" : "1" }
     
-    let staticSource = createStaticDataSource()
-    
-    dataAdapter = CompoundDataAdapter(dataSources: [staticSource, listAdapter])
+    let listAdapter2 = UnorderedListDataAdapter<Int, String>(list: list2)
+    listAdapter2.groupContentsSortingCriteria = { $0 > $1 }
+    listAdapter2.groupingCriteria = { return $0 > 0 ? "2" : "1" }
+
+    dataAdapter = CompoundDataAdapter(dataSources: [listAdapter1, listAdapter2])
     adapter = TableViewAdapter(dataSource: dataAdapter, tableView: tableView)
     
     adapter.registerCellClass(TestCell.self)
     adapter.nibNameForObjectMatching = { _ in String(describing: TestCell.self) }
-    adapter.didSelectCell.subscribeNext { _, _, object in print(object) }.ownedBy(self)
-  }
-  
-  private func createStaticDataSource() -> ObjectsDataSource<Int> {
-    let source = StaticDataSource<Int>()
-    source.sections = [StaticObjectsSection(title: nil, objects: [10, 20, 30])]
-    
-    return source
   }
   
   @IBAction private func addMore(sender: AnyObject?) {
-    list.performUpdates { $0.delete([1, 2, 3]) }
+    list1.performUpdates { $0.insert([6]) }
+    list2.performUpdates { $0.insert([-151, 12]) }
   }
   
 }
